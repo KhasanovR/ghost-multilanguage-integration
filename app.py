c861f80a8c32b063229625faeaed92048e13cb71
KhasanovR <u1610183@gmail.com>
2020-02-03 20:04:04 +0500

    add some minor changes to previous commit

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ from views import translate_post, get_translations
 
 app = Flask(__name__)
 
-
-# app.add_url_rule('/translate_post', 'publish', methods=['POST'], view_func=translate_post)
-app.add_url_rule('/get_translations', 'publish', methods=['POST'], view_func=get_translations)
+app.add_url_rule('/translate_post',  methods=['POST'], view_func=translate_post)
+app.add_url_rule('/get_translations', methods=['POST'], view_func=get_translations)
